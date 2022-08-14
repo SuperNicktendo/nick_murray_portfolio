f49808ab6ebb85b73f6d27da89da8e69ecc84fac
Nick Murray <nicholas.murray1332@googlemail.com>
2022-08-14 18:05:32 +0100

    updated title

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header>Site Under Construction</header>
-      <iframe src="https://giphy.com/embed/f9RGISl9RmNOsWR5NP" width="320" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/car-robot-fly-f9RGISl9RmNOsWR5NP">via GIPHY</a></p>
+      <header>Under Construction</header>
+      <iframe src="https://giphy.com/embed/f9RGISl9RmNOsWR5NP" width="320" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
     </div>
   );
 }
