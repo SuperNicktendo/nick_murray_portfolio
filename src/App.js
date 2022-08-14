79b2ecccc27f1c785003dac0c14f31e281d1f2d8
Nick Murray <nicholas.murray1332@googlemail.com>
2022-08-14 18:06:54 +0100

    another commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ function App() {
   return (
     <div className="App">
       <header>Under Construction</header>
-      <iframe src="https://giphy.com/embed/f9RGISl9RmNOsWR5NP" width="320" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
     </div>
   );
 }
